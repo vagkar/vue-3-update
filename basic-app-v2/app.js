1b8f1b8b118de2f363ab87c53427f179e6050aea
Vaggelis Karampoulis <vaggos971@gmail.com>
2025-05-02 01:18:49 +0300

    328. Important Changes & Migration Steps

--- basic-app-v2/app.js
+++ basic-app-v2/app.js
@@ -3,18 +3,18 @@ Vue.component('the-button', {
   methods: {
     updateMessage() {
       this.$emit('update');
-    },
-  },
+    }
+  }
 });
 
 new Vue({
   el: '#app',
   data: {
-    message: 'This works in Vue 2!',
+    message: 'This works in Vue 2!'
   },
   methods: {
     changeMessage() {
       this.message = 'Will it work in Vue 3?';
-    },
-  },
+    }
+  }
 });
--- basic-app-v3/app.js
+++ basic-app-v3/app.js
@@ -1,20 +1,22 @@
-Vue.component('the-button', {
-  template: '<button @click="updateMessage">Click me</button>',
-  methods: {
-    updateMessage() {
-      this.$emit('update');
-    },
-  },
-});
-
-new Vue({
-  el: '#app',
-  data: {
-    message: 'This works in Vue 2!',
+const app = Vue.createApp({
+  data() {
+    return { message: 'This works in Vue 2!' };
   },
   methods: {
     changeMessage() {
       this.message = 'Will it work in Vue 3?';
-    },
-  },
+    }
+  }
 });
+
+app.component('the-button', {
+  emits: ['update'],
+  template: '<button @click="updateMessage">Click me</button>',
+  methods: {
+    updateMessage() {
+      this.$emit('update');
+    }
+  }
+});
+
+app.mount('#app');
